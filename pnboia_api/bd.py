16832a53cf02c63f8f19b20651fadf354c232d89
tobias ferreira <soutobias@yahoo.com.br>
2022-07-04 17:56:35 -0300

    fastapi remobs

--- pnboia_api/bd.py
+++ pnboia_api/bd.py
@@ -6,6 +6,7 @@ import os
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import urllib
+from urllib.parse import quote  
 
 class GetData():
 
@@ -14,30 +15,34 @@ class GetData():
   def __init__(self):
     self.engine = GetData.engine_create()
 
-  def get(self, table, buoy_id=None, start_date=None, end_date=None, last=None, **kwargs):
+  def get(self, table, arguments=None):
 
-    if table != 'buoys':
-      if start_date == None:
-        start_date = (datetime.utcnow() - timedelta(days=3)).strftime('%Y-%m-%d')
-      if end_date == None:
-        end_date = (datetime.utcnow() + timedelta(days=1)).strftime('%Y-%m-%d')
-      query = f"SELECT * FROM {table} WHERE buoy_id = {buoy_id} AND date_time > '{start_date}' AND date_time < '{end_date}'"
-    else:
-      query = f"SELECT * FROM {table} WHERE true"
-
-    if kwargs:
-      query = self.create_query(query, kwargs)
+    query = f"SELECT * FROM {table}"
+    if arguments:
+      query = self.create_query(query, arguments)
 
     df = pd.read_sql(query, self.engine)
 
     return df
 
-  def post(self,table, df, data_type=None):
 
-    if table == 'data_buoys':
-      station = list(df.station_id.astype('str').unique())
+  def post(self,table, data):
 
-    df.to_sql(con=self.engine, name=table, if_exists='append', index=False)
+    df = pd.DataFrame(data)
+    print(data)
+    if table == 'buoy':
+      buoy = self.get(table=table, arguments={'id': df['buoy_id']})
+      if buoy.empty:
+        df.to_sql(con=self.engine, name=table, if_exists='append', index=False)
+        return {
+            "status" : "SUCCESS",
+            "data" : df.to_json()
+        }
+      else:
+        return {
+            "status" : "ERRO",
+            "code" : "Boia já está no banco de dados"
+        }
 
   def delete(self, table, **kwargs):
 
@@ -51,20 +56,22 @@ class GetData():
     self.post(table=table, df=df, data_type=data_type)
 
 
-  def engine_create():
+  def engine_create(qc=True):
 
-    password = os.getenv('POSTGRE_PWD')
-    engine = create_engine(f"postgresql+psycopg2://{os.getenv('POSTGRE_USER')}:{password}@{os.getenv('POSTGRE_LOCAL')}/{os.getenv('POSTGRE_BD')}")
+    if qc:
+      password = os.getenv('REMOBS_QC_DB_PASSWORD')
+      local = os.getenv('REMOBS_QC_DB_URL')
+      engine = create_engine(f"postgresql+psycopg2://{os.getenv('REMOBS_QC_DB_USR')}:{quote(password)}@{local}/{os.getenv('REMOBS_QC_DB')}")
+
     return engine
 
   def create_query(self, query, kwargs):
+    x = 0
     for key, value in kwargs.items():
-      if type(value[1]) == list:
-        if len(value[1]) == 1:
-          query += f" AND {key} {value[0]} ('{value[1][0]}')"
-        else:
-          query += f" AND {key} {value[0]} {tuple(value[1])}"
+      if x==0:
+        query += f" WHERE {key}='{value}'"
+        x +=1
       else:
-        query += f" AND {key} {value[0]} '{value[1]}'"
-
+        query += f" AND {key}='{value}'"
+      
     return query
