9a91c16afa1ef01b079e43078eb2d1f0e6a92d15
Thiago Caminha <caminha.thiago@gmail.com>
2024-04-26 10:05:51 -0300

    first metadata content version finished

--- pnboia_api/app/utils.py
+++ pnboia_api/app/utils.py
@@ -77,23 +77,30 @@ class APIUtils:
     """
         return base_html
 
-    def compose_buoy_information(self, setup_buoys, buoy_parameters):
-        buoys_information_html = "</ul><p>CONFIGURAÇÃO DE SENSORES:</p><ul>"
+    def compose_buoy_information(self, setup_buoys, buoy_parameters, buoy_type):
+        buoys_information_html = ""
 
-        if "wspd2" in buoy_parameters:
-            buoys_information_html += f"<li>Altura do Anemômetro 1 (sensor 1): {setup_buoys[0].height_anemometer_1} m</li>"
-            buoys_information_html += f"<li>Altura do Anemômetro 2 (sensor 2): {setup_buoys[0].height_anemometer_2} m</li>"
+        if buoy_type in ("SPOTTER","TRIAXYS"):
+            buoys_information_html += "</ul>"
+            return buoys_information_html
 
-        if "cspd1" in buoy_parameters:
-            buoys_information_html += f"<li>Tamanho célula ADCP: {setup_buoys[0].cell_size_adcp} m</li>"
-            buoys_information_html += f"<li>Profundidade Inicial (limite superior primeira celula) ADCP: {setup_buoys[0].depth_adcp} m</li>"
+        elif buoy_type == "METOCEAN":
+            buoys_information_html += "</ul><p>CONFIGURAÇÃO DE SENSORES:</p><ul>"
+
+            if "wspd2" in buoy_parameters:
+                buoys_information_html += f"<li>Altura do Anemômetro 1 (sensor 1): {setup_buoys[0].height_anemometer_1} m</li>"
+                buoys_information_html += f"<li>Altura do Anemômetro 2 (sensor 2): {setup_buoys[0].height_anemometer_2} m</li>"
+
+            if "cspd1" in buoy_parameters:
+                buoys_information_html += f"<li>Tamanho célula ADCP: {setup_buoys[0].cell_size_adcp} m</li>"
+                buoys_information_html += f"<li>Profundidade Inicial (limite superior primeira celula) ADCP: {setup_buoys[0].depth_adcp} m</li>"
 
 
-        buoys_information_html += "</ul>"
+            buoys_information_html += "</ul>"
 
         return buoys_information_html
 
-    def list_parameters(self, parameters, buoy_parameters):
+    def list_parameters(self, parameters, buoy_parameters, buoy_type):
         parameters_html = f"""<p>PARAMETROS:</p><ul>"""
 
         params_dict = {}
@@ -101,11 +108,45 @@ class APIUtils:
             if param.parameter in buoy_parameters:
                 params_dict.update({param.id: f"<li>{param.parameter}: {param.description}</li>"})
 
+            if buoy_type == "METOCEAN" and any(item in param.parameter for item in ["cspd", "cdir"]):
+                params_dict.update({param.id: f"<li>{param.parameter}: {param.description}</li>"})
+
+
         keys_list = list(params_dict.keys())
         keys_list.sort()
-        print(keys_list)
+
+        params_dict.update({(max(keys_list) + 1): f'<li>flag_[parâmetro]* : coluna que reúne flags indicativas do controle de qualidade referente ao respectivo parâmetro. A relação de flags pode ser consultada na sessão "CONTROLE DE QUALIDADE".</li></ul>'})
+
+        keys_list = list(params_dict.keys())
+        keys_list.sort()
+
         sorted_params_texts = [params_dict[i] for i in keys_list]
         for text in sorted_params_texts:
             parameters_html += text
 
+
+
         return parameters_html
+
+
+
+    def compose_quality_control_section(self):
+        quality_control_html = """<p>CONTROLE DE QUALIDADE:</p><ul>
+        <li> Flag 0: dado considerado saudável;</li>
+        <li> Flag 1  (miss_value, hard flag): valores faltosos (nulos);</li>
+        <li> Flag 2 (gross_range, hard flag): valores que estão acima ou abaixo dos limites de detecção do respectivo sensor. Tais valores são filtrados (i.e. transformados em -9999);</li>
+        <li> Flag 6 (stuck_sensor, soft flag): marca valores que se repetem 7 vezes consecutivas, indicando possível travamento do sensor naquele período;</li>
+        <li> Flag 8 (time_continuity, soft flag): marca valores anômalos em comparação aos valores vizinhos, indicando possível inconsistência temporal;</li>
+        <li> Flag 9 (fine_range, soft flag): marca possíveis outliers em relação aos dados históricos (PNBoia) da região da bóia.</li></ul>
+
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Detalhes sobre os procedimentos do controle de qualidade podem ser conultados na <a href="https://www.marinha.mil.br/chm/sites/www.marinha.mil.br.chm/files/u1947/controle_de_qualidade_dos_dados.pdf">Documentação do Controle de Qualidade</a>.
+        """
+
+        return quality_control_html
+
+    def compose_observations_section(self):
+        observations_html = """<p>OBSERVAÇÕES:</p><ul>
+  <li> Atentar ao início/fim de cada série temporal, uma vez que podem compreender dados de trajeto do navio de operação de lançamento/recolhimento da boia;</li>
+  <li> As soft flags (Flags 6, 8 e 9) são sugestões de controle de qualidade, ficando a cargo do usuário utilizá-las para filtragem dos dados;</li>
+        """
+        return observations_html
---
+++ pnboia_api/app/v1/metadata.py
@@ -0,0 +1,151 @@
+import pandas as pd
+import numpy as np
+
+from typing import Optional, Any, List
+from fastapi import APIRouter, Query, Depends, HTTPException
+from fastapi.responses import PlainTextResponse, JSONResponse, HTMLResponse
+
+from sqlalchemy.orm import Session
+
+from pnboia_api.schemas.qualified_data import *
+from pnboia_api.schemas.moored import *
+
+from pnboia_api.models.qualified_data import *
+
+import pnboia_api.crud as crud
+
+from  pnboia_api.db.base import Base, engine
+from datetime import datetime, timedelta, date
+
+from pnboia_api.app.deps import get_db
+
+from pnboia_api.app.utils import APIUtils
+
+Base.metadata.create_all(bind=engine)
+
+router = APIRouter()
+
+#######################
+# QUALIFIED_DATA.QualifiedData ENDPOINT
+#######################
+
+@router.get("", status_code=200, response_class=HTMLResponse) #response_model=List[SetupBuoySchema]) #response_class=PlainTextResponse)
+def return_metadata(
+            buoy_id: int,
+            token: str,
+            db: Session = Depends(get_db),
+            response_type:str="plain"
+):
+
+    user = crud.crud_adm.user.verify(db=db, arguments={'token=': token})
+
+    buoy = crud.crud_moored.buoy.show(db=db, id_pk = buoy_id)
+
+    if not buoy.open_data and not user.user_type == 'admin':
+        if not user.user_type == 'admin':
+            raise HTTPException(
+                status_code=400,
+                detail="You do not have permission to do this action",
+            )
+
+    buoy_type = buoy.name.split(" ")[0]
+
+    setup_buoys = crud.crud_moored.setup_buoy.index(db=db)
+    buoys_metadata = crud.crud_moored.buoys_metadata.index(db=db, arguments={'buoy_id=': buoy_id})
+
+
+    parameters = crud.crud_moored.parameters.index(db=db)
+    if buoy_type == "SPOTTER":
+        buoy_params = list(SpotterQualifiedSchema.__fields__.keys())
+    if buoy_type == "METOCEAN":
+            buoy_params = list(BMOBrQualifiedSchema.__fields__.keys())
+    if buoy_type == "TRIAXYS":
+            buoy_params = list(TriaxysQualifiedSchema.__fields__.keys())
+
+
+    base_html = APIUtils().compose_base_html(buoy=buoy, buoys_metadata=buoys_metadata)
+    buoy_information = APIUtils().compose_buoy_information(setup_buoys=setup_buoys, buoy_parameters=buoy_params, buoy_type=buoy_type)
+    parameters_html = APIUtils().list_parameters(parameters=parameters, buoy_parameters=buoy_params, buoy_type=buoy_type)
+    quality_control_html = APIUtils().compose_quality_control_section()
+    observations_html = APIUtils().compose_observations_section()
+
+    final_html = base_html + buoy_information + parameters_html + quality_control_html + observations_html
+    # with open("test", "w") as f:
+    #     f.write(base_text)
+
+    return final_html
+
+
+
+# @router.get("/pnboia", status_code=200, response_model=List[PNBoiaQualifiedSchema])
+# def qualified_data_index(
+#         buoy_id: int,
+#         token: str,
+#         start_date: Optional[str] = Query(default=(datetime.utcnow().replace(microsecond=0) - timedelta(days=1)),
+#                     title="date_time format is yyyy-mm-ddTHH:MM:SS",
+#                     regex="\d{4}-\d?\d-\d?\dT(?:2[0-3]|[01]?[0-9]):[0-5]?[0-9]:[0-5]?[0-9]"),
+#         end_date: Optional[str] = Query(default=(datetime.utcnow().replace(microsecond=0) + timedelta(days=2)),
+#                     title="date_time format is yyyy-mm-ddTHH:MM:SS",
+#                     regex="\d{4}-\d?\d-\d?\dT(?:2[0-3]|[01]?[0-9]):[0-5]?[0-9]:[0-5]?[0-9]"),
+#         db: Session = Depends(get_db),
+#         limit: int = None,
+#         response_type:str="json"
+#     ) -> Any:
+
+#     user = crud.crud_adm.user.verify(db=db, arguments={'token=': token})
+
+#     arguments = {}
+#     try:
+#         start_date = datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%S")
+#         end_date = datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%S")
+#     except:
+#         start_date = datetime.combine(start_date, datetime.min.time())
+#         end_date = datetime.combine(end_date, datetime.min.time())
+
+#     if end_date < start_date:
+#         raise HTTPException(
+#                 status_code=400,
+#                 detail="Provided start date is more recent then the end date.",
+#             )
+
+#     if start_date > datetime.utcnow():
+#         start_date = (datetime.utcnow() - timedelta(days=3))
+#     if start_date >= end_date:
+#         raise HTTPException(
+#                 status_code=400,
+#                 detail=f"Provided start date is more recent than the provided end date. Please review your requested period.",
+#             )
+#     if (end_date - start_date).days > 30:
+#         end_date = (start_date + timedelta(days=30))
+
+
+#     arguments = {'buoy_id=': buoy_id, 'date_time>=': start_date.strftime("%Y-%m-%d"), 'date_time<=': end_date.strftime("%Y-%m-%d")}
+
+#     buoy = crud.crud_moored.buoy.show(db=db, id_pk = buoy_id)
+
+#     if "METOCEAN" not in buoy.name:
+#         raise HTTPException(
+#                 status_code=400,
+#                 detail=f"Please check in the PNBoia documentation if the correct endpoint is being used for the required buoy (Buoy ID = {buoy_id}).",
+#             )
+
+#     if not buoy.open_data and not user.user_type == 'admin':
+#         if not user.user_type == 'admin':
+#             raise HTTPException(
+#                 status_code=400,
+#                 detail="You do not have permission to do this action",
+#             )
+#     else:
+#         result = crud.crud_qualified_data.pnboia_qualified_data.index(db=db, order=True, arguments=arguments, limit=limit)
+
+#     if not result:
+#         raise HTTPException(
+#                 status_code=400,
+#                 detail=f"No data for buoy {buoy_id} for the period.",
+#             )
+
+#     if response_type == "csv":
+#         filename = APIUtils().file_name_composition(buoy_name=buoy.name, start_date=start_date, end_date=end_date)
+#         return APIUtils().csv_response(result=result, filename=filename)
+#     elif response_type == "json":
+#         return result
