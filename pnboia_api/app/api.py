60a3e34c3acfeaf580691e0a5aef1da818059119
Thiago Caminha <caminha.thiago@gmail.com>
2024-05-08 10:27:49 -0300

    info superendpoint created, encompassing metadata and available_buoys endpoints

--- pnboia_api/app/api.py
+++ pnboia_api/app/api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 
-from pnboia_api.app.v1 import auth, moored, drift, qualified_data, quality_control, metadata
+from pnboia_api.app.v1 import auth, info, moored, drift, qualified_data, quality_control
 
 
 app = FastAPI(title="PNBOIA API", openapi_url="/openapi.json")
@@ -31,7 +31,7 @@ async def root():
 app.include_router(moored.router, prefix="/v1/moored", tags=["moored"])
 app.include_router(drift.router, prefix="/v1/drift", tags=["drift"])
 app.include_router(qualified_data.router, prefix="/v1/qualified_data", tags=["qualified_data"])
-app.include_router(metadata.router, prefix="/v1/metadata", tags=["metadata"])
+app.include_router(info.router, prefix="/v1/info", tags=["info"])
 app.include_router(quality_control.router, prefix="/v1/quality_control", tags=["quality_control"])
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 
